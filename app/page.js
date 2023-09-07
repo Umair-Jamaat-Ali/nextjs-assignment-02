2ee142f5bc01d10db7dd8106f90aae4f22c8aaa7
UMAIR JAMAAT ALI <umairansari758@gmail.com>
2023-09-07 23:27:34 +0500

    delete button

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 'use client'
-import {useState} from "react";
+import { useState } from "react";
 export default function Home() {
 
   const [data, setData] = useState([]);
 
   const dataLoader = async () => {
-    let response = await fetch ('https://api.github.com/users/naveed-rana/followers');
+    let response = await fetch('https://api.github.com/users/naveed-rana/followers');
 
     response = await response.json();
     setData(response);
@@ -13,23 +13,44 @@ export default function Home() {
     // console.log("response data", response.data);
   }
 
-  return (
-   <>
-   <button className=" w-20 h-10 border-2 border-blue-400 m-10 rounded-xl hover:bg-blue-500 hover:text-white hover:border-black" onClick={dataLoader}> 
-    Loaded
-   </button>
+  const deleteHandler = (logInName) => {
+    let filterDeleteName = data.filter((item) => item.login !== logInName)
+    return setData([data, ...filterDeleteName]);
+  }
 
-   <ul className="m-10">
-    {data.map((item, index)=>{
-      return(
-        <li key="index">
-          {item.login}
-          <br />
-          {item.followers_url}
-        </li> 
-      )
-    })}
-   </ul>
-   </>
+  return (
+    <>
+      <button
+        className=" w-20 h-10 border-2
+    border-blue-400
+     m-10 rounded-xl
+      hover:bg-blue-500
+       hover:text-white
+        hover:border-black"
+        onClick={dataLoader}>
+        Loaded
+      </button>
+
+      <ul className="m-10">
+        {data.map((item, index) => {
+          return (
+            <li key="index">
+              {item.login}
+              <br />
+              {item.followers_url}
+              <button
+                className=" w-20 h-10 border-2
+           border-blue-400 m-10 
+           rounded-xl
+            hover:bg-blue-500 
+            hover:text-white 
+            hover:border-black"
+                onClick={() => deleteHandler(item.login)}
+              >Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
